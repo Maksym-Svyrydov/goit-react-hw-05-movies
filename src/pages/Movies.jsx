4bfb71980a0e5e12df679f8a5995aecc0e1fb1d5
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-03-19 18:13:37 +0200

    layout&fetch

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,16 +1,19 @@
-// import { useEffect } from 'react';
-// import SearchBox from 'components/Search';
-// import MoviesList from '../components/MovieList';
-// import { useSearchParams } from 'react-router-dom';
-// const Movies = () => {
-//   const [searchParams, setSearchParams] = useSearchParams();
-//   const query = searchParams.get('query');
-//   useEffect(() => {});
-//   return (
-//     <div class="">
-//       <SearchBox />
-//       <MoviesList />
-//     </div>
-//   );
-// };
-// export default Movies;
+import { useEffect, useState } from 'react';
+import SearchBox from 'components/Search';
+import MoviesList from '../components/MovieList';
+import { useSearchParams } from 'react-router-dom';
+
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('[]');
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {});
+  return (
+    <div class="">
+      <SearchBox />
+      <MoviesList />
+    </div>
+  );
+};
+export default Movies;
