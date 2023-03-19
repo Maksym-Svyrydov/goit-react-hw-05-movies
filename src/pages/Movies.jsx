ba34e269d63013029f3b27e85e7e508855578e5e
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-03-19 18:21:16 +0200

    layout&home

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import SearchBox from 'components/Search';
 import MoviesList from '../components/MovieList';
-import { useSearchParams } from 'react-router-dom';
+// import { useSearchParams } from 'react-router-dom';
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState('[]');
-  const [searchParams, setSearchParams] = useSearchParams();
+  // const [movies, setMovies] = useState([]);
+  // const [query, setQuery] = useState('[]');
+  // const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {});
   return (
